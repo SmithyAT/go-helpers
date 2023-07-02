c985822dcf6b69da8706a43f0782af3e27605457
Christian Schmied <30844629+SmithyAT@users.noreply.github.com>
2023-07-02 21:27:50 +0200

    Add new logger with multiple output support
    This commit introduces a new logging system to the project. The logger has support for console, logfile, and Papertrail output. This provides flexibility in choosing the logging method based on different scenarios. The new logger also supports the configuration of various parameters, such as output levels, file size for rotation, and age of files for backup. This will improve the logging system's efficiency and customization options.

---
+++ go.mod
@@ -0,0 +1,32 @@
+module github.com/smithyat/go-helpers
+
+go 1.20
+
+require (
+	github.com/antonfisher/nested-logrus-formatter v1.3.1
+	github.com/minio/minio-go/v7 v7.0.59
+	github.com/polds/logrus-papertrail-hook v0.0.0-20180214143432-bcfe7b72c1a4
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
+	github.com/sirupsen/logrus v1.9.3
+	github.com/snowzach/rotatefilehook v0.0.0-20220211133110-53752135082d
+	golang.org/x/sys v0.9.0
+)
+
+require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/minio/sha256-simd v1.0.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/rs/xid v1.5.0 // indirect
+	github.com/stvp/go-udp-testing v0.0.0-20201019212854-469649b16807 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+)
---
+++ logger/logger.go
@@ -0,0 +1,175 @@
+package logger
+
+import (
+	"fmt"
+	nested "github.com/antonfisher/nested-logrus-formatter"
+	logruspapertrail "github.com/polds/logrus-papertrail-hook"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
+	"github.com/snowzach/rotatefilehook"
+	"golang.org/x/sys/unix"
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+// Log is the global logger
+var Log *logrus.Logger
+
+// LoggingConfig represents the configuration for the logging mechanism of the application.
+type LoggingConfig struct {
+	Debug        bool     // if true, log level will be set to DEBUG
+	Logfile      string   // empty to disable logfile
+	MaxSize      int      // rotate after x MB filesize
+	MaxAge       int      // rotate evey x days
+	Backup       int      // number of backups to keep
+	PTLogLevel   string   // <DEBUG|INFO|WARNING|ERROR> or empty to disable PT logging
+	PTAppName    string   // if empty name of executable will be used
+	PTSourceHost string   // Which host should be shown in PaperTrail
+	PTHost       string   // PaperTrail host
+	PTPort       int      // PaperTrail port
+	FieldsOrder  []string // order of fields in log output
+}
+
+// Isatty Check if a TTY is attached
+func Isatty() bool {
+	_, err := unix.IoctlGetWinsize(int(os.Stdout.Fd()), unix.TIOCGWINSZ)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// Trace returns a string with useful trace information used in error handling
+func Trace() string {
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+	funcName := strings.SplitN(frame.Function, "/", 2)
+	return fmt.Sprintf("%s %s:%d", funcName[len(funcName)-1], filepath.Base(frame.File), frame.Line)
+}
+
+// InitLogger Initialize a new logger and set it as the global logger
+func InitLogger(cfg LoggingConfig) {
+	Log = newLogger(cfg)
+}
+
+// newLogger Initialize a new logger
+func newLogger(cfg LoggingConfig) *logrus.Logger {
+	newLogger := logrus.New()
+	newLogger.SetOutput(io.Discard)
+	if cfg.Debug {
+		newLogger.SetLevel(logrus.DebugLevel)
+	} else {
+		newLogger.SetLevel(logrus.InfoLevel)
+	}
+
+	// Set defaults
+	if cfg.MaxSize == 0 {
+		cfg.MaxSize = 10
+	}
+
+	if cfg.MaxAge == 0 {
+		cfg.MaxAge = 30
+	}
+
+	if cfg.Backup == 0 {
+		cfg.Backup = 6
+	}
+
+	// ########################################################################
+	// Output to console
+	// ########################################################################
+	if Isatty() {
+		newLogger.Hooks.Add(lfshook.NewHook(
+			os.Stdout,
+			&nested.Formatter{
+				NoColors:         false,
+				HideKeys:         true,
+				NoFieldsColors:   false,
+				NoFieldsSpace:    false,
+				ShowFullLevel:    false,
+				NoUppercaseLevel: false,
+				TrimMessages:     true,
+				TimestampFormat:  "15:04:05",
+				FieldsOrder:      cfg.FieldsOrder,
+			},
+		))
+	}
+
+	// ########################################################################
+	// Output to logfile
+	// ########################################################################
+	if cfg.Logfile != "" {
+		rotateFileHook, err := rotatefilehook.NewRotateFileHook(rotatefilehook.RotateFileConfig{
+			Filename:   cfg.Logfile,
+			MaxSize:    cfg.MaxSize,
+			MaxBackups: cfg.Backup,
+			MaxAge:     cfg.MaxAge,
+			Level:      logrus.DebugLevel,
+			Formatter: &nested.Formatter{
+				HideKeys:         true,
+				NoColors:         true,
+				NoFieldsColors:   false,
+				NoFieldsSpace:    false,
+				ShowFullLevel:    false,
+				NoUppercaseLevel: false,
+				TrimMessages:     true,
+				TimestampFormat:  "2006-01-02 15:04:05",
+				FieldsOrder:      cfg.FieldsOrder,
+			},
+		})
+		if err != nil {
+			logrus.Fatalf("Failed to initialize logfile: %v", err)
+		}
+		newLogger.AddHook(rotateFileHook)
+	}
+
+	// ########################################################################
+	// Output to Papertrail
+	// ########################################################################
+	if cfg.PTLogLevel != "" {
+		newLogger.SetFormatter(&logrus.TextFormatter{
+			DisableTimestamp: true,
+			ForceColors:      false,
+		})
+
+		var appname string
+		if cfg.PTAppName == "" {
+			filename, _ := os.Executable()
+			appname = filepath.Base(filename)
+		} else {
+			appname = cfg.PTAppName
+		}
+
+		var hook *logruspapertrail.Hook
+		hook, err := logruspapertrail.NewPapertrailHook(&logruspapertrail.Hook{
+			Host:     "logs2.papertrailapp.com",
+			Port:     39413,
+			Hostname: "bigdata01",
+			Appname:  appname,
+		})
+
+		switch strings.ToUpper(cfg.PTLogLevel) {
+		case "DEBUG":
+			hook.SetLevels([]logrus.Level{logrus.DebugLevel, logrus.InfoLevel, logrus.WarnLevel, logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel})
+		case "INFO":
+			hook.SetLevels([]logrus.Level{logrus.InfoLevel, logrus.WarnLevel, logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel})
+		case "WARNING":
+			hook.SetLevels([]logrus.Level{logrus.WarnLevel, logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel})
+		case "ERROR":
+			hook.SetLevels([]logrus.Level{logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel})
+		default:
+			hook.SetLevels([]logrus.Level{})
+		}
+
+		if err == nil {
+			newLogger.AddHook(hook)
+		}
+	}
+
+	return newLogger
+}
