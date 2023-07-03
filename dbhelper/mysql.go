9baf7f39826e09a6cb8efb8303d8aa38379899d9
Christian Schmied <30844629+SmithyAT@users.noreply.github.com>
2023-07-03 15:49:35 +0200

    Add MySQL database helper functions
    This commit introduces a suite of helper functions to interface with MySQL databases. The new file `mysql.go` in the `dbhelper` package includes functions to assemble a database configuration struct, generate a Database Source Name (DSN) from that configuration, mask the password in such a DSN, and establish a connection to a MySQL database using a given DSN. These functions aim to simplify and streamline interactions with MySQL databases in the application.

---
+++ dbhelper/mysql.go
@@ -0,0 +1,55 @@
+package dbhelper
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/smithyat/go-helpers/logger"
+	"regexp"
+	"strings"
+	"time"
+)
+
+// MySqlConfig is the configuration for the MySQL database
+type MySqlConfig struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+	Database string
+}
+
+// MySqlDSN returns the DSN string for the MySQL database
+func MySqlDSN(dbConfig MySqlConfig) (dsn string) {
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?timeout=5s&autocommit=true",
+		dbConfig.Username, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Database)
+}
+
+// MySqlDSNMaskPassword returns the DSN string for the MySQL database with the password masked
+func MySqlDSNMaskPassword(s string) string {
+	re, err := regexp.Compile(`(.*?):(.*?)@`)
+	if err != nil {
+		return ""
+	}
+
+	return re.ReplaceAllStringFunc(s, func(str string) string {
+		parts := strings.Split(str, ":")
+		return fmt.Sprintf("%s:%s@", parts[0], strings.Repeat("*", len(parts[1])-1)) // We subtract 1 from length to account for the "@" character
+	})
+}
+
+// MySQLConnect connects to the MySQL database and returns a pointer to the connection
+func MySQLConnect(dsn string) (db *sqlx.DB, disconnect func(), dbErr error) {
+	mysqlDB, err := sqlx.Connect("mysql", dsn)
+	if err != nil {
+		dbErr = fmt.Errorf("Failed to connect to MySQL database: %v [%s]", err, logger.Trace())
+		return nil, nil, dbErr
+	}
+
+	mysqlDB.SetConnMaxLifetime(time.Minute * 3)
+	mysqlDB.SetMaxOpenConns(5)
+	mysqlDB.SetMaxIdleConns(5)
+
+	return mysqlDB, func() {
+		_ = mysqlDB.Close()
+	}, nil
+}
