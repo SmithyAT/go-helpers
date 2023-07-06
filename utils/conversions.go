310c9e6fd50fc1d6532623224f7a4b62e43ae09a
Christian Schmied <30844629+SmithyAT@users.noreply.github.com>
2023-07-06 21:03:11 +0200

    Update ConvertStrToInt function documentation
    The documentation of the ConvertStrToInt function in utils/conversions.go was updated to explain not only what the function does, but also the arguments it takes and the value it returns. This change enhances the function's clarity and will ease its use.

--- utils/conversions.go
+++ utils/conversions.go
@@ -2,7 +2,17 @@ package utils
 
 import "strconv"
 
-// ConvertStrToInt converts a string to an integer
+// ConvertStrToInt takes a string as input and converts it to an integer.
+// If the string cannot be converted to an integer, the function returns 0.
+// The function uses the Atoi function from the strconv package for conversion.
+//
+// Parameters:
+//
+//	s: a string value that should be converted to integer
+//
+// Returns:
+//
+//	an integer representing the string input, or 0 if conversion is not possible
 func ConvertStrToInt(s string) int {
 	i, err := strconv.Atoi(s)
 	if err != nil {
